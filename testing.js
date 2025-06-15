a3ba02067c90f5194cb44e98c730f11ac85e36e2
ISHANT BHOYAR <95231976+ISHANT57@users.noreply.github.com>
2025-06-15 19:13:46 +0530

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,44 @@
+// for (let i=0 ; i<=10 ; i++){
+//     console.log(`outer loop is: ${i}`)
+//     for (let j=0 ; j<=10; j++){
+//         // 
+//         console.log( i + "*" + j + "=" + i*j)
+
+//     }
+
+// }
+
+
+// for (let k=0; k<=10; k++){
+//     console.log(` k runs from 0 to 10: ${k}`);
+    
+// }
+
+// let i =0
+// while ( i<=10){
+//     console.log(`value of index is ${i}`);
+//     i+=2
+    
+// }
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+// const arr=[1,2,3,4,5]
+// for (const i of arr) {
+//     console.log(i);
+    
+    
+// }
+
+const mynums=[ 1,2,3,4,5]
+
+mynums.filter( (num)=> {
+    num >4
+} )
+
+console.log(mynums);
+
